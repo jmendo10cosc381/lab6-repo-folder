e117d70cd58d93e1d98b562f39d0417b48167fd6
Julia Mendoza <231963277+jmendo10cosc381@users.noreply.github.com>
2025-09-30 18:32:54 +0000

    Created the test_sample_run_anonymizer method

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -3,4 +3,11 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
     # replace the following line with your test
-    pass
+    first= {
+        "text": "My name is Bip",
+        "items": [
+            {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+        ]
+    }
+    result = sample_run_anonymizer("My name is Bond",11,15)
+    assert first.__eq__(result)
